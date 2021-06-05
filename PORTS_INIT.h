293d259aec12c9745507702f0ad69667de20cf52
monica-zakaria <67977568+monica-zakaria@users.noreply.github.com>
2021-06-06 00:08:22 +0200

    initialization of ports

---
+++ PORTS_INIT.h
@@ -0,0 +1,10 @@
+#include "stdint.h"
+#include "E:/keil/EE319Kware/inc/tm4c123gh6pm.h"
+#define PORTA  1
+#define PORTB  2
+#define PORTC  3
+#define PORTD  4
+#define PORTE  5
+#define PORTF  6
+
+void init(uint8_t number);
