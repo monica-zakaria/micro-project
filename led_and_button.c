fdd762f8282439c0790e3b84e673a4961fb2f40b
marina1518 <72987033+marina1518@users.noreply.github.com>
2021-06-06 00:26:18 +0200

    LEDS_BUTTONS_FUNCTIONS

---
+++ led_and_button.c
@@ -0,0 +1,22 @@
+
+#include "led_and_button.h"
+
+void led_button_init (void)
+	{
+		init(PORTF);
+  
+	}
+	
+	void led1_on (uint8_t distance ) {
+		if (distance >= 100){ 
+	GPIO_PORTF_DATA_R = 0x02 ; //led1 , pf1 ,[red led]
+	}
+}
+	
+	
+	uint8_t get_button1_value(void){
+	 return GPIO_PORTF_DATA_R &= 0x10  ; // pf4 switch1 for start 
+	}
+	uint8_t get_button2_value(void){
+	 return GPIO_PORTF_DATA_R &= 0x01  ; // pf1 switch2 foe end 
+	}
